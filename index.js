fa2d669d86225cd50d9c4e8b4cc2c34a02e8f549
hassanlmezher <hassanmezher084@gmail.com>
2025-07-24 11:06:45 +0300

    falling meadow

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-// When the user clicks the purchase button, render out
-// "Something went wrong, please try again" in the paragraph
-// that has the id="error".
-let errorP = document.getElementById('error');
-
-
-function wrong() {
-    errorP.innerText = "Something went wrong, please try again";
+let num1 = 8;
+let num2 = 2;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+let sumEL = document.getElementById('sum-el');
+function add() {
+    sumEL.textContent = "Sum: ";
+    sumEL.textContent += num1 + num2;
+}
+function subtract() {
+    sumEL.textContent = "Sum: ";
+    sumEL.textContent += num1 - num2;
+}
+function devide() {
+    sumEL.textContent = "Sum: ";
+    sumEL.textContent += num1 / num2;
+}
+function multiply() {
+    sumEL.textContent = "Sum: ";
+    sumEL.textContent += num1 * num2;
 }
