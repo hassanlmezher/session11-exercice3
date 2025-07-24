81512305a0ae4d74c355d6b16a221b398ae7b1ca
hassanlmezher <hassanmezher084@gmail.com>
2025-07-24 10:52:13 +0300

    Incrementing and decrementing

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-let namee = "Linda";
-let greeting = "Hi there";
-function phrase() {
-    console.log(greeting + ", " + namee + " !");
+let myPoints = 3;
+function add3Points() {
+    myPoints += 3;
+}
+function remove1Point(){
+    myPoints -= 1;
 }
 
-phrase();
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+console.log(myPoints);
